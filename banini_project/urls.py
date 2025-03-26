3e426973cf0ea27a12e0b6c1ec85b8a309763d05
Ricardo Romanini <ricardoromanini9@gmail.com>
2025-03-26 09:38:55 -0300

    Corrige rotas do sistema BANINI

--- banini_project/urls.py
+++ banini_project/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('banini_app.urls')),
+    path('', include('banini_app.urls')),  # Página inicial redireciona para o app
+    path('pagamentos/', include('banini_app.urls')),
 ]
