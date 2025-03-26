677319104cb419e8684c9f8ab3cf4cfeaa65a566
Ricardo Romanini <ricardoromanini9@gmail.com>
2025-03-26 10:33:06 -0300

    Corrige redirecionamento para lista de pagamentos

--- banini_project/urls.py
+++ banini_project/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', views.lista_pagamentos, name='lista_pagamentos'),
     path('admin/', admin.site.urls),
     path('', include('banini_app.urls')),  # Página inicial redireciona para o app
     path('pagamentos/', include('banini_app.urls')),
